be0c3a14ee8490afba52fd043ff1a84ba33537aa
vicevil77 <120662253+vicevil77@users.noreply.github.com>
2024-04-24 21:10:25 +0200

    a

--- app_model.py
+++ app_model.py
@@ -59,8 +59,8 @@ def retrain(): # Rutarlo al endpoint '/api/v1/retrain/', metodo GET
 @app.route('/webhook_2024', methods=['POST'])
 def webhook():
     # Ruta al repositorio donde se realizará el pull
-    path_repo = '/ruta/a/tu/repositorio/en/PythonAnywhere'
-    servidor_web = '/ruta/al/fichero/WSGI/de/configuracion' 
+    path_repo = "https://github.com/vicevil77/taller_despliegue.git"
+    servidor_web = '/var/www/vicevil_pythonanywhere_com_wsgi.py' 
 
     # Comprueba si la solicitud POST contiene datos JSON
     if request.is_json:
